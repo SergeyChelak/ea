9446fcb9b9f68c3050ff9141e2cf9734d31497c6
Sergey Chelak <schelak@lanapro.com>
2020-03-20 18:05:42 +0200

    updated type converter

--- app/src/main/java/org/chelak/ea/database/UserDatabase.kt
+++ app/src/main/java/org/chelak/ea/database/UserDatabase.kt
@@ -6,25 +6,23 @@ import org.chelak.ea.common.BitMask
 import org.chelak.ea.database.dao.EstateDao
 import org.chelak.ea.database.entity.Estate
 
-@TypeConverters(Converters::class)
+@TypeConverters(UserDatabase.Addons::class)
 @Database(entities = [Estate::class],
     exportSchema = false,
     version = 1)
 abstract class UserDatabase : RoomDatabase() {
 
-    companion object {
+    companion object Addons {
         fun create(context: Context, file: String): UserDatabase = Room.databaseBuilder(
             context, UserDatabase::class.java, file
         ).build()
+
+        @TypeConverter
+        fun toBitMask(value: Int): BitMask = BitMask(value)
+
+        @TypeConverter
+        fun toInt(bitMask: BitMask?): Int = bitMask?.raw ?: 0
     }
 
     abstract fun estateDao(): EstateDao
-}
-
-private class Converters {
-    @TypeConverter
-    fun toBitMask(value: Int): BitMask = BitMask(value)
-
-    @TypeConverter
-    fun toInt(bitMask: BitMask?): Int = bitMask?.raw ?: 0
 }
