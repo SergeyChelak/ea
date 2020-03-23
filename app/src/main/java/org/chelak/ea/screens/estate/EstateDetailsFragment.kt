1d0399046f8a6421eab2dcfbca06a624b47d0e34
Sergey Chelak <schelak@lanapro.com>
2020-03-23 03:23:38 +0200

    removed useless fragment

--- app/src/main/java/org/chelak/ea/screens/estate/EstateDetailsFragment.kt
+++ app/src/main/java/org/chelak/ea/screens/estate/EstateDetailsFragment.kt
@@ -1,16 +1,13 @@
 package org.chelak.ea.screens.estate
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
-
 import org.chelak.ea.R
-import org.chelak.ea.common.Logger
 import org.chelak.ea.ui.MainActivity
 
 class EstateDetailsFragment : Fragment() {
@@ -29,7 +26,6 @@ class EstateDetailsFragment : Fragment() {
         view.let {
             val button: Button? = it?.findViewById(R.id.dbg_button_meter_list)
             button?.setOnClickListener { _ ->
-//                nc.navigate(R.id.action_estateDetailsFragment_to_meterListFragment)
                 viewModel.openMeterList()
             }
         }
