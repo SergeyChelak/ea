9c0c3da99118673f2ac9dc187e938e1dd6f00c00
Sergey Chelak <schelak@lanapro.com>
2020-05-27 15:58:43 +0300

    code refactored

--- app/src/main/java/org/chelak/ea/ui/list/ArrayListAdapter.kt
+++ app/src/main/java/org/chelak/ea/ui/list/ArrayListAdapter.kt
@@ -4,16 +4,16 @@ import androidx.recyclerview.widget.RecyclerView
 
 abstract class ArrayListAdapter<T, VH: RecyclerView.ViewHolder> : RecyclerView.Adapter<VH>() {
 
-    private var items: List<T>? = null
+    private var items: List<T> = emptyList()
 
-    fun replace(items: List<T>?) {
+    fun replace(items: List<T>) {
         this.items = items
         notifyDataSetChanged()
     }
 
-    override fun getItemCount(): Int = items?.size ?: 0
+    override fun getItemCount(): Int = items.size
 
-    operator fun get(position: Int): T = items!![position]
+    operator fun get(position: Int): T = items[position]
 
     // TODO implement insert/delete/tale and head append
 }
