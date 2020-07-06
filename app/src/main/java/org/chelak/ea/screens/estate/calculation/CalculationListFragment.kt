e7b7ce7e1587d942bc28bd56c3e4efd717f5487b
Sergey Chelak <schelak@lanapro.com>
2020-07-07 01:23:50 +0300

    initialized recycler view

--- app/src/main/java/org/chelak/ea/screens/estate/calculation/CalculationListFragment.kt
+++ app/src/main/java/org/chelak/ea/screens/estate/calculation/CalculationListFragment.kt
@@ -7,11 +7,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.estate_list_fragment.*
 import org.chelak.ea.R
 import org.chelak.ea.ui.MainActivity
 import org.chelak.ea.ui.estateId
 import org.chelak.ea.ui.list.ArrayListAdapter
 import org.chelak.ea.ui.list.CaptionValueViewHolder
+import org.chelak.ea.ui.list.clickPosition
+import org.chelak.ea.ui.list.setVerticalLayout
 
 class CalculationListFragment : Fragment() {
 
@@ -40,6 +44,18 @@ class CalculationListFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        view?.let {
+            val recyclerView = it.findViewById<RecyclerView>(R.id.recyclerView)
+            recyclerView.setVerticalLayout()
+            recyclerView.adapter = adapter
+            recyclerView.clickPosition().observe(viewLifecycleOwner, Observer {
+                //
+            })
+
+            appendButton.setOnClickListener {
+
+            }
+        }
         (activity as? MainActivity)?.component?.inject(viewModel)
         arguments?.estateId?.let {
             viewModel.setEstateId(it)
